bff54b74b52de16faecb6c35cbd4ba61739fdbbb
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    sdm660-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- sdm660.mk
+++ sdm660.mk
@@ -371,6 +371,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile  
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
